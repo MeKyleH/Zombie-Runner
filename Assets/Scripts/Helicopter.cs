b3a5373be545c187ca5c0f776c5c00d33c5d3b35
meKyleh <kyle.a.holt@gmail.com>
2016-03-26 16:22:07 -0600

    helicopter comes to flare

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -5,14 +5,26 @@ public class Helicopter : MonoBehaviour {
 
 	private bool called = false;
 	private Rigidbody rigidBody;
+	private Transform targetLocation;
+	private GameObject targetObject;
+	private float speed = 30f;
 
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> ();
 	}
 
+	void Update() {
+		if (called) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, targetLocation.position, step);
+		}
+	}
+
 	void OnDispatchHelicopter() {
 		print ("heli called");
-		rigidBody.velocity = new Vector3 (0, 0, 50f);
+		targetObject = GameObject.FindGameObjectWithTag ("Finish");
+		targetLocation = targetObject.transform;
+		//rigidBody.velocity = new Vector3 (0, 0, 50f);
 		called = true;
 	}
 }
