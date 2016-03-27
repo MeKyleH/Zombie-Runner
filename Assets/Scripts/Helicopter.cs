839738c9f5db925f9d14ffb2c2bf7de2fe95b237
meKyleh <kyle.a.holt@gmail.com>
2016-03-26 22:12:59 -0600

    updating to real zombies

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -4,14 +4,9 @@ using System.Collections;
 public class Helicopter : MonoBehaviour {
 
 	private bool called = false;
-	private Rigidbody rigidBody;
 	private Transform targetLocation;
 	private GameObject targetObject;
 	private float speed = 30f;
-
-	void Start () {
-		rigidBody = GetComponent<Rigidbody> ();
-	}
 
 	void Update() {
 		if (called) {
@@ -24,7 +19,6 @@ public class Helicopter : MonoBehaviour {
 		print ("heli called");
 		targetObject = GameObject.FindGameObjectWithTag ("Finish");
 		targetLocation = targetObject.transform;
-		//rigidBody.velocity = new Vector3 (0, 0, 50f);
 		called = true;
 	}
 }
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+	private Transform player;
+	private Animator animator;
+	private Rigidbody rigidBody;
+	private Vector3 startSpeed; 
+	private NavMeshAgent navMeshAgent;
+
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		startSpeed = navMeshAgent.velocity;
+		rigidBody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Player") {
+			print ("attacking");
+			animator.SetTrigger ("isAttacking");
+			navMeshAgent.velocity = Vector3.zero;
+		}
+	}
+
+	void OnTriggerStay(Collider collider) {
+		if (collider.tag == "Player") {
+			print ("attacking");
+			animator.SetTrigger ("isAttacking");
+			navMeshAgent.velocity = Vector3.zero;
+		}
+	}
+
+	/*
+	 void OnTriggerExit(Collider collider) {
+		if (collider.tag == "Player") {
+			animator.SetBool ("isAttacking", false);
+			print ("not attacking");
+			//navMeshAgent.velocity = startSpeed;
+		}
+	}*/
+}
