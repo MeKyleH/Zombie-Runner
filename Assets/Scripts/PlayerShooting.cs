4094043567cb6ed1c94daa67d1c37fb12081995d
meKyleh <kyle.a.holt@gmail.com>
2016-03-28 23:32:06 -0600

    added gun to view and shooting

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public ParticleSystem muzzleFlash;
+	Animator animator;
+	public ParticleSystem impactFlash;
+
+	void Start () {
+		animator = GetComponentInChildren<Animator> ();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			muzzleFlash.Play ();
+			animator.SetTrigger ("firingTrigger");
+			RaycastHit hit;
+
+
+			if (Physics.Raycast (transform.position, transform.forward, out hit, 50f)) {
+				
+				if (hit.transform.tag == "Enemy") {
+					Destroy (hit.transform.gameObject);
+				}
+				impactFlash.transform.position = hit.point;
+				impactFlash.Play ();
+			}
+		}
+
+	}
+}
