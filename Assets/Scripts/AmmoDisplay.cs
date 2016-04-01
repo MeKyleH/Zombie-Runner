2c4c8b0759abbc6e7f999cd2aeb541944ce6b522
meKyleh <kyle.a.holt@gmail.com>
2016-03-31 22:27:14 -0600

    adding ammo

---
+++ Assets/Scripts/AmmoDisplay.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class AmmoDisplay : MonoBehaviour {
+
+	private Text ammoText;
+	private int ammo = 10;
+	public enum Status {SUCCESS, FAILURE};
+
+	void Start () {
+		ammoText = gameObject.GetComponent<Text> ();
+		UpdateDisplay ();
+	}
+
+	public void AddAmmo(int amount) {
+		ammo += amount;
+		UpdateDisplay ();
+	}
+
+	public Status UseAmmo(int amount) {
+		if (ammo >= amount) {
+			ammo -= amount;
+			UpdateDisplay ();
+			return Status.SUCCESS;
+		}
+		return Status.FAILURE;
+	}
+
+	private void UpdateDisplay(){
+		if (ammoText) {
+			ammoText.text = ammo.ToString ();
+		} else {
+			ammoText = GameObject.FindObjectOfType<Text> ();
+			if (ammoText) {
+				ammoText.text = ammo.ToString ();
+			} else {
+				Debug.Log ("Error finding ammoText");
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -8,31 +8,35 @@ public class PlayerShooting : MonoBehaviour {
 	public ParticleSystem impactFlash;
 	public AudioClip gunshot;
 
+	private AmmoDisplay ammoDisplay;
 	private AudioSource audioSource;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
 		animator = GetComponentInChildren<Animator> ();
+		ammoDisplay = GameObject.FindObjectOfType<AmmoDisplay>();
 	}
 	
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			muzzleFlash.Play ();
-			audioSource.clip = gunshot;
-			audioSource.Play ();
-			animator.SetTrigger ("firingTrigger");
-
-			// shooting
-			RaycastHit hit;
-			if (Physics.Raycast (transform.position, transform.forward, out hit, 50f)) {
-				
-				if (hit.transform.tag == "Enemy") {
-					Destroy (hit.transform.gameObject);
-				}
-				impactFlash.transform.position = hit.point;
-				impactFlash.Play ();
-			}
+		if (Input.GetButtonDown ("Fire1") && ammoDisplay.UseAmmo(1) == AmmoDisplay.Status.SUCCESS) {
+			Shoot();
 		}
+	}
 
+	void Shoot() {
+		muzzleFlash.Play ();
+		audioSource.clip = gunshot;
+		audioSource.Play ();
+		animator.SetTrigger ("firingTrigger");
+
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 50f)) {
+			
+			if (hit.transform.tag == "Enemy") {
+				Destroy (hit.transform.gameObject);
+			}
+			impactFlash.transform.position = hit.point;
+			impactFlash.Play ();
+		}
 	}
 }
