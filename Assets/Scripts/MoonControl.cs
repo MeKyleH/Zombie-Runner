2a6ef6382f3be23396d222f246c74596dbf19751
meKyleh <kyle.a.holt@gmail.com>
2016-04-01 23:16:22 -0600

    adding moon

---
+++ Assets/Scripts/MoonControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonControl : MonoBehaviour {
+
+	public float distance = 1000f;
+	public float scale = 15f;
+
+	void Start () {
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, distance);
+		transform.localScale = new Vector3 (scale, scale, scale);
+	}
+}
